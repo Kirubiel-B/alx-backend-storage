bf579a542814d0c74b9e0e7bfc57d012305dcfe0
kirubel beza <kirubelkeb@gmail.com>
2024-05-15 03:36:08 -0700

    redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
-"""module for the function get_page"""
-
-import requests
+"""
+Caching request module
+"""
 import redis
+import requests
+from functools import wraps
+from typing import Callable
 
-client = redis.Redis()
+
+def track_get_page(fn: Callable) -> Callable:
+    """ Decorator for get_page
+    """
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """ Wrapper that:
+            - check whether a url's data is cached
+            - tracks how many times get_page is called
+        """
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
+    return wrapper
 
 
+@track_get_page
 def get_page(url: str) -> str:
-    """obtain the HTML content of a particular URL and returns it."""
-    result = requests.get(url).text
-    if not client.get("count:{}".format(url)):
-        client.set("count:{}".format(url), 1)
-        client.setex("result:{}".format(url), 10, result)
-    else:
-        client.incr("count:{}".format(url), 1)
-    return result
+    """ Makes a http request to a given endpoint
+    """
+    response = requests.get(url)
+    return response.text
